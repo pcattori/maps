bd5450b06dc0c010648a9e46b37f1eb4c278592b
Pedro Cattori <pcattori@gmail.com>
2017-01-21 00:51:17 -0500

    namedmap replaced with namedfrozen and namedfixedkey

--- tests/test_init.py
+++ tests/test_init.py
@@ -2,10 +2,12 @@ import maps
 import unittest
 
 class TestInit(unittest.TestCase):
-    def test_namedmap(self):
-        RGB = maps.namedmap('RGB', ['red', 'green', 'blue'])
+    def test_namedfrozen(self):
+        RGB = maps.namedfrozen('RGB', ['red', 'green', 'blue'])
         self.assertEqual(type(RGB), maps.NamedFrozenMapMeta)
-        CMYK = maps.namedmap('CMYK', ['cyan', 'magenta', 'yellow', 'black'], fixed_keys=True)
+
+    def test_namedfixedkey(self):
+        CMYK = maps.namedfixedkey('CMYK', ['cyan', 'magenta', 'yellow', 'black'])
         self.assertEqual(type(CMYK), maps.NamedFixedKeyMapMeta)
 
 if __name__ == '__main__':
