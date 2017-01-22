1b670b39cac08f95cd83b9c547d39dbb7e199cba
Pedro Cattori <pcattori@gmail.com>
2017-01-22 12:25:48 -0500

    docstring for fixedkeymap class

--- maps/fixedkeymap.py
+++ maps/fixedkeymap.py
@@ -1,6 +1,28 @@
 import collections.abc
 
 class FixedKeyMap(collections.abc.MutableMapping):
+    '''A key-value mapping with a fixed set of keys whose items are accessible
+    via bracket-notation (i.e. ``__getitem__`` and ``__setitem__``). Though the
+    set of keys is immutable, the corresponding values can be edited.
+
+    :param args: Position arguments in the same form as the :py:class:`dict` constructor.
+    :param kwargs: Keyword arguments in the same form as the :py:class:`dict` constructor.
+
+    Usage::
+
+       >>> import maps
+       >>> fkm = maps.FixedKeyMap({'a': 1, 'b': 2})
+       >>> fkm['a']
+       1
+       >>> fkm['b'] += 10
+       >>> fkm['b']
+       12
+       >>> list(fkm.items())
+       [('a', 1), ('b', 12)]
+       >>> len(fkm)
+       2
+    '''
+
     def __init__(self, *args, **kwargs):
         self._data = dict(*args, **kwargs)
 
