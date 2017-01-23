bf5decf95ad48baefc1f763c19139617eaeb2a78
Pedro Cattori <pcattori@gmail.com>
2017-01-23 16:44:40 -0500

    include code in docs

--- docs/conf.py
+++ docs/conf.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-# import os
-# import sys
-# sys.path.insert(0, os.path.abspath('.'))
+import os
+import sys
+sys.path.insert(0, os.path.abspath('..'))
 
 # -- General configuration ------------------------------------------------
 
